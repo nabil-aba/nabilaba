644c74d76784333445648b91069deb7e12b6aa71
nabil-aba <nabilazizbimaangita1@gmail.com>
2026-08-01 08:10:46 +0700

    coba

--- vite.config.js
+++ vite.config.js
@@ -3,37 +3,55 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "ssg-preview-router",
-      configurePreviewServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url.includes(".")) {
-            return next();
-          }
+export default defineConfig(({ isSsrBuild }) => {
+  return {
+    plugins: [
+      react(),
+      {
+        name: "ssg-preview-router",
+        configurePreviewServer(server) {
+          server.middlewares.use((req, res, next) => {
+            if (req.url.includes(".")) return next();
+            const url = req.url.replace(/\/$/, "") || "/";
 
-          const url = req.url.replace(/\/$/, "") || "/";
+            if (url === "/") req.url = "/index.html";
+            else if (url === "/id") req.url = "/id/index.html";
+            else if (url.startsWith("/id/")) req.url = "/id/404.html";
+            else req.url = "/404.html";
 
-          if (url === "/") {
-            req.url = "/index.html";
-          } else if (url === "/id") {
-            req.url = "/id/index.html";
-          } else if (url.startsWith("/id/")) {
-            req.url = "/id/404.html";
-          } else {
-            req.url = "/404.html";
-          }
-
-          next();
-        });
+            next();
+          });
+        },
+      },
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: isSsrBuild
+            ? undefined
+            : {
+                "vendor-react": [
+                  "react",
+                  "react-dom",
+                  "react-router-dom",
+                  "react-helmet-async",
+                ],
+                "vendor-chakra": [
+                  "@chakra-ui/react",
+                  "@emotion/react",
+                  "@emotion/styled",
+                ],
+                "vendor-framer": ["framer-motion"],
+                "vendor-icons": ["react-icons"],
+              },
+        },
+      },
+      chunkSizeWarningLimit: 600,
     },
-  },
+  };
 });
